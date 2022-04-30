85d24a81b158c12641adbc922f588da38b6b2e02
Stephanie Thomas <stephie.thomas@gmail.com>
2022-04-30 12:51:22 -0500

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,28 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const Header = () => {
  return(
-     <h1> Hello World </h1>
- )
-
+     <section className="header_section">
+         <div className="header">
+        <h1> Stephanie Thomas </h1>
+        </div>
+        <nav className="nav">
+            <ul>
+                <Link to="/">
+                 <li>About</li>   
+                </Link>
+                <Link to="/contact">
+                    <li>Contact</li>
+                </Link>
+                <Link to="/resume">
+                    <li>Resume</li>
+                </Link>                
+            </ul>
+        </nav>
+     </section>
+ ); 
+ 
 }
 
 export default Header;
