9b1341cae65c3a5e4c38e1f6e604c09bbf8e902b
Stephanie Thomas <stephie.thomas@gmail.com>
2022-04-30 11:54:02 -0500

    setting up files structure

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../components/Header";
+import About from "../components/About";
+import Work from "../components/Work";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Work />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
