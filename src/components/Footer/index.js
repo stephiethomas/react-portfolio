43b63c7682dabd27b0ae849e46e608c5ab54a945
Stephanie Thomas <stephie.thomas@gmail.com>
2022-04-30 12:40:40 -0500

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <ul>
+                <li>
+                    <a href="https://github.com/stephiethomas" target={"_blank"}>
+                        GitHub
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/stephaniethomas/" target={"_blank"}>
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:stephie.thomas02@gmail.com" target={'_blank'}>
+                        Email
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+export default Footer;
